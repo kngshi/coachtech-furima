56f162de4376d0547f6275f8b029a52eb1a703a1
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-09-17 17:33:32 +0900

    add:ER図の作成

--- src/database/migrations/2024_07_16_212436_create_payment_methods_table.php
+++ src/database/migrations/2024_07_16_212436_create_payment_methods_table.php
@@ -15,7 +15,7 @@ class CreatePaymentMethodsTable extends Migration
     {
         Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',191);
             $table->timestamps();
         });
     }
