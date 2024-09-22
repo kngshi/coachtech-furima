1b801b216ac467334303ad5ca13db275b8852a49
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-09-22 18:33:48 +0900

    fix:payment-method.blade.phpのインデント修正

--- src/resources/views/payment-method.blade.php
+++ src/resources/views/payment-method.blade.php
@@ -7,30 +7,29 @@
 @section('content')
 <div class="container">
     <h1 class="payment-method__ttl">支払い方法の変更</h1>
-
     <form action="{{ route('payment.method.update', ['item' => $item->id]) }}" method="POST">
         @csrf
         <input type="hidden" name="item_id" value="{{ request('item') }}">
         <div class="form-group">
-        <div class="payment-method__message">支払い方法を選択してください</div>
-        <div class="payment-method__label">
-            <label>
-                <input type="radio" name="payment_method_id" value="1" {{ $paymentMethod == 1 ? 'checked' : '' }}>
-                クレジットカード
-            </label>
-        </div>
-        <div class="payment-method__label">
-            <label>
-                <input type="radio" name="payment_method_id" value="2" {{ $paymentMethod == 2 ? 'checked' : '' }}>
-                コンビニ払い
-            </label>
-        </div>
-        <div class="payment-method__label">
-            <label>
-                <input type="radio" name="payment_method_id" value="3" {{ $paymentMethod == 3 ? 'checked' : '' }}>
-                銀行振込
-            </label>
-        </div>
+            <div class="payment-method__message">支払い方法を選択してください</div>
+            <div class="payment-method__label">
+                <label>
+                    <input type="radio" name="payment_method_id" value="1" {{ $paymentMethod == 1 ? 'checked' : '' }}>
+                    クレジットカード
+                </label>
+            </div>
+            <div class="payment-method__label">
+                <label>
+                    <input type="radio" name="payment_method_id" value="2" {{ $paymentMethod == 2 ? 'checked' : '' }}>
+                    コンビニ払い
+                </label>
+            </div>
+            <div class="payment-method__label">
+                <label>
+                    <input type="radio" name="payment_method_id" value="3" {{ $paymentMethod == 3 ? 'checked' : '' }}>
+                    銀行振込
+                </label>
+            </div>
         </div>
         <button type="submit" class="btn btn-primary">変更を保存</button>
     </form>
